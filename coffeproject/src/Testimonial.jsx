65863c47be4c166ea31c4d2e3bc45ab9702d4898
student-creative <aryasutariya3@gmail.com>
2025-06-12 23:47:39 -0700

    new link

--- coffeproject/src/Testimonial.jsx
+++ coffeproject/src/Testimonial.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import Slider from './Slider'
-import Navbar from './Navbar'
-import Client from './Client'
+import React from 'react';
+import Slider from './Slider';
+import Navbar from './Navbar';
+import Client from './Client';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
-
 
 function Testimonial() {
   return (
     <div>
-         <Slider />
-            <Navbar />
-            <div className="absolute top-1/4 w-full flex flex-col items-center justify-center text-white z-10 px-4">
-                <div className="menu-bar">
-                    <h1 className='text-7xl uppercase'>Testimonial</h1>
-                </div>
-                <div className="navbar space-x-3 text-xl mt-3">
-                    <Link className='text-white no-underline hover:underline' to="/">Home </Link>
-                    <a href="" className='text-white no-underline'>/</a>
-                    <a className='no-underline text-white' href="">Testimonial</a>
-                </div>
-            </div>
+      <Slider />
+      <Navbar />
+      <div className="absolute top-1/4 w-full flex flex-col items-center justify-center text-white z-10 px-4">
+        <div className="menu-bar">
+          <h1 className='text-7xl uppercase'>Testimonial</h1>
+        </div>
+        <div className="navbar space-x-3 text-xl mt-3">
+          <Link className='text-white no-underline hover:underline' to="/">Home</Link>
+          <span className='text-white'> / </span>
+          <span className='no-underline text-white'>Testimonial</span>
+        </div>
+      </div>
 
-        <Client/>
-        <Footer/>
+      <Client />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Testimonial
+export default Testimonial;
