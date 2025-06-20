15272efe4187ea4443ba8d67a75f0b067c1e6e75
student-creative <aryasutariya3@gmail.com>
2025-06-20 12:26:59 -0700

    Minor Change

--- blogpost/src/Navbar.jsx
+++ blogpost/src/Navbar.jsx
@@ -5,17 +5,36 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
       <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">📝 BlogVerse</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold" to="/">
+          📝 BlogVerse
+        </Link>
+
+        {/* Toggler for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav">
+
+        {/* Collapsible menu */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto text-center text-lg-start">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link fw-semibold text-white" to="/">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/add">Add Blog</Link>
+              <Link className="nav-link fw-semibold text-white" to="/add">
+                Add Blog
+              </Link>
             </li>
           </ul>
         </div>
