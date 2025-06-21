73cd89144c1ac125660a6df72656892b3dc4f78a
student-creative <aryasutariya3@gmail.com>
2025-06-21 03:25:05 -0700

    Respnsive Change

--- coffeproject/src/Client.jsx
+++ coffeproject/src/Client.jsx
@@ -1,57 +1,65 @@
-import React from 'react'
+import React from 'react';
 
 function Client() {
   return (
-    <div>
-      <div className="client py-5 px-3">
-        <div className=" mx-5 px-3">
-          <div className="pt-24 pb-5">
-            <h4 className="text-center text-orange-400 text-3xl">T&nbsp;e&nbsp;s&nbsp;t&nbsp;i&nbsp;m&nbsp;o&nbsp;n&nbsp;i&nbsp;a&nbsp;l&nbsp;</h4>
-            <h1 class="text-center text-7xl">Our Clients Say</h1>
-          </div>
+    <div className="client py-12 px-4 sm:px-6 lg:px-10 bg-white">
+      {/* Section Header */}
+      <div className="text-center mb-12">
+        <h4 className="text-orange-400 text-2xl sm:text-3xl tracking-widest">
+          T&nbsp;e&nbsp;s&nbsp;t&nbsp;i&nbsp;m&nbsp;o&nbsp;n&nbsp;i&nbsp;a&nbsp;l
+        </h4>
+        <h1 className="text-4xl sm:text-5xl font-bold text-[#2D3B36]">Our Clients Say</h1>
+      </div>
 
-          <div className="person flex">
-            <div className="item1 flex flex-wrap">
-              <img className='w-[80px] h-[80px] ' src={require('./img/person.jpg')} alt="" />
-              <div className="heading ml-3">
-
-                <h4>Client Name</h4>
-                <i>Profession</i>
-
-              </div>
-              <p class="m-0 font-montserrat">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
+      {/* Clients Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+        {/* Client 1 */}
+        <div className="flex flex-col items-start">
+          <div className="flex items-center mb-4">
+            <img className="w-20 h-20 rounded-full" src={require('./img/person.jpg')} alt="Client 1" />
+            <div className="ml-4">
+              <h4 className="text-lg font-semibold">Client Name</h4>
+              <i className="text-sm">Profession</i>
             </div>
-         
-          
-            <div className="item1 flex flex-wrap">
-              <img className='w-[80px] h-[80px] ' src={require('./img/person2.jpg')} alt="" />
-              <div className="heading ml-3">
+          </div>
+          <p className="text-sm font-montserrat text-[#2D3B36] hidden sm:block">
+            Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem.
+            Dolor tempor ipsum sanct clita.
+          </p>
+        </div>
 
-                <h4>Client Name</h4>
-                <i>Profession</i>
-
-              </div>
-              <p class="m-0 font-montserrat">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
+        {/* Client 2 */}
+        <div className="flex flex-col items-start">
+          <div className="flex items-center mb-4">
+            <img className="w-20 h-20 rounded-full" src={require('./img/person2.jpg')} alt="Client 2" />
+            <div className="ml-4">
+              <h4 className="text-lg font-semibold">Client Name</h4>
+              <i className="text-sm">Profession</i>
             </div>
-
-         
-         
-            <div className="item1 flex flex-wrap">
-              <img className='w-[80px] h-[80px] ' src={require('./img/person3.jpg')} alt="" />
-              <div className="heading ml-3">
-
-                <h4>Client Name</h4>
-                <i>Profession</i>
-
-              </div>
-              <p class="m-0 font-montserrat">Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem. Dolor tempor ipsum sanct clita</p>
-           
           </div>
+          <p className="text-sm font-montserrat text-[#2D3B36] hidden sm:block">
+            Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem.
+            Dolor tempor ipsum sanct clita.
+          </p>
+        </div>
+
+        {/* Client 3 */}
+        <div className="flex flex-col items-start">
+          <div className="flex items-center mb-4">
+            <img className="w-20 h-20 rounded-full" src={require('./img/person3.jpg')} alt="Client 3" />
+            <div className="ml-4">
+              <h4 className="text-lg font-semibold">Client Name</h4>
+              <i className="text-sm">Profession</i>
+            </div>
           </div>
+          <p className="text-sm font-montserrat text-[#2D3B36] hidden sm:block">
+            Sed ea amet kasd elitr stet, stet rebum et ipsum est duo elitr eirmod clita lorem.
+            Dolor tempor ipsum sanct clita.
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Client
+export default Client;
--- coffeproject/src/Testimonial.jsx
+++ coffeproject/src/Testimonial.jsx
@@ -13,27 +13,26 @@ function Testimonial() {
   }, []);
 
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Slider />
       <Navbar />
 
-      {/* Header Section with Animation */}
+      {/* Header Section */}
       <div
-        className="absolute top-1/4 w-full flex flex-col items-center justify-center text-white z-10 px-4"
+        className="absolute top-1/4 w-full flex flex-col items-center justify-center text-white z-10 px-4 text-center"
         data-aos="fade-down"
       >
-        <div className="menu-bar">
-          <h1 className='text-7xl uppercase'>Testimonial</h1>
-        </div>
-        <div className="navbar space-x-3 text-xl mt-3">
+        <h1 className='text-4xl sm:text-5xl md:text-6xl uppercase font-bold'>Testimonial</h1>
+        <div className="flex flex-wrap justify-center items-center gap-2 text-base sm:text-lg mt-3">
           <Link className='text-white no-underline hover:underline' to="/">Home</Link>
           <span className='text-white'> / </span>
-          <span className='no-underline text-white'>Testimonial</span>
+          <span className='text-white'>Testimonial</span>
         </div>
       </div>
 
-      {/* Client Section (already animated inside if using AOS inside Client.jsx) */}
-      <div data-aos="fade-up">
+      {/* Client Heading */}
+      <div className="w-full py-16 px-4 sm:px-8 text-center" data-aos="fade-up">
+        <h2 className="text-3xl sm:text-4xl font-bold text-[#2D3B36] uppercase mb-8">Client</h2>
         <Client />
       </div>
 
